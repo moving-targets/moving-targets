a61d0c770e83507cd1b3600d5e2b1d38f1bd0513
Luca Giuliani <luca.giuliani10@studio.unibo.it>
2022-01-11 11:54:44 +0100

    Include documentation generation into setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,36 @@
 # Run this script from the origin folder as:
 #   > "python setup.py test" in order to execute all the unittests
 #   > "python setup.py bdist_wheel" in order to build the library
-
+import os
 import shutil
+import sys
 
 from setuptools import find_packages, setup
 
-# clears previous builds
+# clear previous builds and documentation
 shutil.rmtree('build', ignore_errors=True)
 shutil.rmtree('dist', ignore_errors=True)
+shutil.rmtree('docs/html', ignore_errors=True)
+shutil.rmtree('docs/source', ignore_errors=True)
 
-# retrieves the description from the readme file
+# generate new documentation .rst with sphinx-apidoc for main module, then make html
+os.mkdir('docs/source')
+os.system(f'sphinx-apidoc -e -T -M -d 8 -o .\\docs\\source .\\moving_targets')
+os.system('sphinx-build .\\docs .\\docs\\html')
+
+# set up the library metadata and make the build
 with open('README.md', 'r') as readme:
-    long_description = readme.read()
-
-# sets the library metadata up
-setup(
-    name="moving-targets",
-    version="0.1.0",
-    maintainer="Luca Giuliani",
-    maintainer_email="luca.giuliani13@unibo.it",
-    author="University of Bologna - DISI",
-    description="Moving Targets: a framework for constrained machine learning",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_packages(include=['moving_targets*']),
-    python_requires='>=3.7',
-    install_requires=['matplotlib~=3.4.3', 'numpy~=1.21.4', 'pandas~=1.3.4', 'scikit-learn~=1.0.1'],
-    test_suite='test'
-)
+    setup(
+        name='moving-targets',
+        version='0.1.0',
+        maintainer='Luca Giuliani',
+        maintainer_email='luca.giuliani13@unibo.it',
+        author='University of Bologna - DISI',
+        description='Moving Targets: a framework for constrained machine learning',
+        long_description=readme.read(),
+        long_description_content_type='text/markdown',
+        packages=find_packages(include=['moving_targets*']),
+        python_requires='>=3.7',
+        install_requires=['matplotlib~=3.4.3', 'numpy~=1.21.4', 'pandas~=1.3.4', 'scikit-learn~=1.0.1'],
+        test_suite='test'
+    )
